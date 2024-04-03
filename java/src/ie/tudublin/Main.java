79253fb383fead5439286c4f176c6b8c7aaef453
RalphjCruz <ralphjcruz18@gmail.com>
2024-04-03 14:02:55 +0100

    Created intro for visuals

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-        processing.core.PApplet.runSketch(a, new RotatingAudioBands());
+        processing.core.PApplet.runSketch(a, new Seno());
     }
 
     public static void main(String[] args) {
