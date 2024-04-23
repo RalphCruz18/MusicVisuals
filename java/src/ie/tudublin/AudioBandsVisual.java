4b5c40f0843d6a65d8b8ad19a4d3b5f6e353af22
BeroCollege <C22421292@mytudublin.ie>
2024-04-23 12:41:27 +0100

    Updated audioBandsVisual.java

--- java/src/ie/tudublin/AudioBandsVisual.java
+++ java/src/ie/tudublin/AudioBandsVisual.java
@@ -2,26 +2,25 @@ package ie.tudublin;
 
 import processing.core.*;
 
-// This is an example of a visual that uses the audio bands
 public class AudioBandsVisual extends PApplet
 {
-    Seno mv;
+    Seno seno;
     
-    public AudioBandsVisual(Seno mv)
+    public AudioBandsVisual(Seno seno)
     {
-        this.mv = mv;
+        this.seno = seno;
     }
 
     public void render(int bandColor)
     {
-        float gap = mv.width / (float) mv.getBands().length * 4.0f;
-        mv.noStroke();
-        mv.fill(bandColor);
+        float gap = seno.width / (float) seno.getBands().length * 4.0f;
+        seno.noStroke();
+        seno.fill(bandColor);
 
-        for(int i = 0 ; i < mv.getBands().length ; i ++)
+        for(int i = 0 ; i < seno.getBands().length ; i ++)
         {
             //draw rectangle for each band
-            mv.rect((i * gap - gap / 2)-3120, mv.height+2300, gap,-mv.getSmoothedBands()[i] * 1f); 
+            seno.rect((i * gap - gap / 2)-3120, seno.height+2300, gap,-seno.getSmoothedBands()[i] * 1f); 
         }
     }
 }
