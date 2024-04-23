1e47b2ba22604059867531862e987631c91e6bd0
BeroCollege <C22421292@mytudublin.ie>
2024-04-23 12:40:08 +0100

    Changed main.java

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,17 +1,10 @@
 package ie.tudublin;
 
-//import ddf.minim.ugens.Waveform;
-//import example.AudioBandsVisual;
-//import example.CubeVisual;
-//import example.CubeVisual1;
-//import example.MyVisual;
-//import example.RotatingAudioBands;
-
 public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-        processing.core.PApplet.runSketch(a, new Seno());
+        processing.core.PApplet.runSketch(a, new Seno()); //Run Seno.java file
     }
 
     public static void main(String[] args) {
